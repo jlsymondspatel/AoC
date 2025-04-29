40727b1cdd5ae277691d0b31aeb8ba20132118f9
jlsymondspatel <104757721+jlsymondspatel@users.noreply.github.com>
2025-04-29 21:36:52 +0100

    Amended Day 2 Part 2 (Octave)

--- 2024_Octave/02_02/is_rep_safe.m
+++ 2024_Octave/02_02/is_rep_safe.m
@@ -15,10 +15,13 @@ function rep_rep = is_rep_safe (rep)
     rep_rep.mask_min_diff = rep_rep.diffs_abs == 0;
     
     # look at the maximum differences mask
-    rep_rep.mask_max_diff = rep_rep.diffs_abs > 4;
+    rep_rep.mask_max_diff = rep_rep.diffs_abs > 3;
 
-    # make mask for the sign of each difference
-    rep_rep.mask_diff_sign = sign (rep_rep.diffs);
+    # get sign of each difference
+    rep_rep.diffs_sign = sign (rep_rep.diffs);
+
+    # initialise mask of violating diff sign values
+    rep_rep.mask_diffs_sign = [];
 
     ####################
     # Obtaining result #
@@ -34,12 +37,14 @@ function rep_rep = is_rep_safe (rep)
 
     # check the number of violations of changing diff sign according to the
     # majority of diffs that are the same sign
-    if (sum (rep_rep.mask_diff_sign) >= 0) # if the majority of levs are increasing
+    if (sum (rep_rep.diffs_sign) >= 0) # if the majority of levs are increasing
         # count only those diffs that are decreasing
-        rep_rep.viols(3) = sum (rep_rep.mask_diff_sign < 0);
+        rep_rep.mask_diffs_sign = rep_rep.diffs_sign < 0;
+        rep_rep.viols(3) = sum (rep_rep.mask_diffs_sign);
     else # if the majority of levs are decreasing
         # count only those diffs that are increasing
-        rep_rep.viols(3) = sum (rep_rep.mask_diff_sign > 0);
+        rep_rep.mask_diffs_sign = rep_rep.diffs_sign > 0;
+        rep_rep.viols(3) = sum (rep_rep.mask_diffs_sign);
     endif
 
     # check the number of violations and return safe only if viols is 0
